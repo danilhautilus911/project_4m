310c58ba8da860955c10ccb0ee2752e39cddf04b
danilhautilus911 <danil.matlahov@yandex.ru>
2021-05-30 19:32:23 +0300

    Create js.js

---
+++ js.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  $('.button1').mouseover(function () {
+    $('.button_action1').addClass('sizer');
+  });
+  $('.button1').mouseout(function () {
+    $('.button_action1').removeClass('sizer');
+  });
+  $('.button2').mouseover(function () {
+    $('.button_action2').addClass('sizer');
+  });
+  $('.button2').mouseout(function () {
+    $('.button_action2').removeClass('sizer');
+  });
+
+  $('.first_option').mouseover(function () {
+    $('.first_option').css('background-color', '#7FDEFF');
+  });
+  $('.first_option').mouseout(function () {
+    $('.first_option').css('background-color', 'white');
+  });
+  $('.second_option').mouseover(function () {
+    $('.second_option').css('background-color', '#FFDD5B');
+  });
+  $('.second_option').mouseout(function () {
+    $('.second_option').css('background-color', 'white');
+  });
+  $('.third_option').mouseover(function () {
+    $('.third_option').css('background-color', '#FF5959');
+  });
+  $('.third_option').mouseout(function () {
+    $('.third_option').css('background-color', 'white');
+  });
+  $('.fourth_option').mouseover(function () {
+    $('.fourth_option').css('background-color', '#FF83BD');
+  });
+  $('.fourth_option').mouseout(function () {
+    $('.fourth_option').css('background-color', 'white');
+  });
+});
