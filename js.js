b6e40bee97cd2aa85e8810a1f38d6da9efcbcfb4
danilhautilus911 <danil.matlahov@yandex.ru>
2021-06-23 01:25:37 +0700

    Update js.js

--- js.js
+++ js.js
@@ -117,6 +117,9 @@ $(document).ready(function() {
   $('.accordion-item_holder_first').mouseout(function () {
     $('.button6').css('opacity', '0');
   });
+  $('.button6').click(function () {
+    $(this).css('opacity', '1');
+  });
   $('.accordion-item_holder_second').mouseover(function () {
     $('.button7').css('opacity', '1');
   });
@@ -158,6 +161,36 @@ $(document).ready(function() {
     $(this).next('.options').slideToggle(200);
   });
 
+  $('.button_anketa').click(function() {
+    $(this).toggleClass('button_anketa_altering');
+  });
+
+  $('.button-js-campaign').click(function() {
+    $(this).css('background-color', '#FF83BD');
+    $(this).css('border', 'none');
+    $(this).css('color', 'white')
+    $('.main').css('filter', 'blur(1px)');
+    $('.overlay-js-campaign').fadeIn();
+  });
+
+  $('.closepopup-js-campaign').click(function() {
+    $('.button-js-campaign').css('background-color', 'white');
+    $('.button-js-campaign').css('border', '4px solid black');
+    $('.button-js-campaign').css('color', 'black')
+    $('.main').css('filter', 'none');
+    $('.overlay-js-campaign').fadeOut();
+  });
+
+  $(document).mouseup(function (e) {
+  	var popup = $('.popup-js-campaign');
+  	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+  		$('.overlay-js-campaign').fadeOut();
+      $('.button-js-campaign').css('background-color', 'white');
+      $('.button-js-campaign').css('border', '4px solid black');
+      $('.button-js-campaign').css('color', 'black')
+      $('.main').css('filter', 'none');
+  	}
+  });
 
 
   // $('.color_general').mouseover(function(){
